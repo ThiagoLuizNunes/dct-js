7732bccc3aecc59d1855aad929b63b26d89a7b30
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-04-24 02:44:26 -0300

    add dct to image

--- dct.py
+++ dct.py
@@ -1,11 +1,12 @@
-from pydub import AudioSegment
+import numpy as np
 from scipy.io import wavfile
-from scipy import fftpack
 from scipy.fftpack import dct
 from scipy.fftpack import idct
+from PIL import Image
 import array as arr
 import wave, struct
 
+#DCT Audio
 waveFile = wave.open('audio.wav', 'r')
 rate = waveFile.getframerate()
 length = waveFile.getnframes()
@@ -44,3 +45,22 @@ newArrayIDCTRobot = newArrayIDCTRobot.astype('int16')
 wavfile.write('audio-idct.wav', rate , newArrayIDCT)
 wavfile.write('audio-idct-chunked.wav', rate , newArrayIDCTChunked)
 wavfile.write('audio-idct-robot.wav', rate , newArrayIDCTRobot)
+
+#DCT Image
+img = Image.open('lena.bmp')
+arrayImg = np.asarray(img)
+
+newImgDCT = dct(dct(arrayImg.T, norm = 'ortho').T, norm = 'ortho')
+maxCosValueImg = 5
+countCosImg = 0
+
+for i in range(0, len(newImgDCT)):
+  for j in range(0, len(newImgDCT[i])):
+    if newImgDCT[i][j] < maxCosValueImg:
+      newImgDCT[i][j] = 0
+      countCosImg += 1
+
+print('Total cosine deleted: ', countCosImg)
+newImgIDCT = idct(idct(newImgDCT.T, norm = 'ortho').T, norm = 'ortho')
+im = Image.fromarray(newImgIDCT)
+im.show()
