b3eff6ed2e4e24b92a61233b44f1fbdf3cce8a92
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-04-24 00:19:33 -0300

    add dct function

--- split.py
+++ split.py
@@ -1,12 +1,27 @@
 from pydub import AudioSegment
+from scipy.io import wavfile
+from scipy import fftpack
+from scipy.fftpack import dct
+from scipy.fftpack import idct
+import array as arr
+import wave, struct
 
-song = AudioSegment.from_wav("audio.wav")
+waveFile = wave.open('audio.wav', 'r')
+length = waveFile.getnframes()
+arrayAudioFrames = arr.array('i')
 
-_previous = 0
-_next = 1
+for i in range(0,length):
+  waveData = waveFile.readframes(1)
+  data = struct.unpack("<h", waveData)
+  arrayAudioFrames.append(int(data[0]))
 
-for x in range(0, 3000):
-  sample = song[_previous : _next]
-  sample.export('./samples/sample' + str(_previous) +'.wav', format="wav")
-  _previous += 1
-  _next += 1
+newArrayDCT = dct(arrayAudioFrames)
+newArrayIDCT = idct(newArrayDCT)
+
+wavfile.write('newaudio.wav', 50000 , newArrayIDCT)
+
+# for x in range(0, 3000):
+#   sample = song[_previous : _next]
+#   sample.export('./samples/sample' + str(_previous) +'.wav', format="wav")
+#   _previous += 1
+#   _next += 1
