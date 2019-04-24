5efa1959bd2c70dac521311ea42b5ff3fc8b59bc
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-04-24 01:12:05 -0300

    add idct conversion

--- split.py
+++ split.py
@@ -6,22 +6,32 @@ from scipy.fftpack import idct
 import array as arr
 import wave, struct
 
+# rate, audioData = wavfile.read('audio.wav')
+# newDCTAudio = dct(audioData)
+# print(newDCTAudio)
+
 waveFile = wave.open('audio.wav', 'r')
+rate = waveFile.getframerate()
 length = waveFile.getnframes()
 arrayAudioFrames = arr.array('i')
 
 for i in range(0,length):
   waveData = waveFile.readframes(1)
-  data = struct.unpack("<h", waveData)
-  arrayAudioFrames.append(int(data[0]))
+  data = struct.unpack('<h', waveData)
+  # arrayAudioFrames.append(int(data[0]))
+  arrayAudioFrames.append(abs(int(data[0])))
 
-newArrayDCT = dct(arrayAudioFrames)
-newArrayIDCT = idct(newArrayDCT)
+newArrayDCT = dct(arrayAudioFrames, norm = 'ortho')
+# for i in range(0, len(newArrayDCT)):
+#   newArrayDCT[i] = abs(newArrayDCT[i])
 
-wavfile.write('newaudio.wav', 50000 , newArrayIDCT)
+newArrayIDCT = idct(newArrayDCT, norm = 'ortho')
+newArrayIDCT = newArrayIDCT.astype('int16')
+# for i in range(0, len(newArrayIDCT)):
+#   newArrayIDCT[i] = abs(newArrayIDCT[i])
 
-# for x in range(0, 3000):
-#   sample = song[_previous : _next]
-#   sample.export('./samples/sample' + str(_previous) +'.wav', format="wav")
-#   _previous += 1
-#   _next += 1
+print(newArrayDCT)
+print(newArrayIDCT)
+
+wavfile.write('audio-dct.wav', rate , newArrayDCT)
+wavfile.write('audio-idct.wav', rate , newArrayIDCT)
