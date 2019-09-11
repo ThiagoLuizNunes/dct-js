b1b3856b0dd3dd3b4370988757d25b4e91cc88d4
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-09-11 17:06:54 -0300

    update dct function and gitignore

--- dct.py
+++ dct.py
@@ -7,53 +7,59 @@ import array as arr
 from modules import helper as hp
 
 
-def applyDCTinAudio(signal, amount=0):
-    N = len(signal)
-    cosines = np.zeros(N).astype(float)
-
-    for k in range(0, N):
-        alpha = sqrt(0.5) if k == 0 else 1
-        Ak = sqrt(2/N)
-        xn = signal[k]
-        samples = np.zeros(N).astype(float)
-
-        for n in range(0, N):
-            samples[n] = Ak * alpha * xn * \
-                cos(radians(((2*pi*k*n)/2*N) + ((k*pi)/2*N)))
-            cosines[n] = cosines[n] + samples[n]
-
-    hp.coeffStrategyOne(cosines, amount)
-
-    return cosines
-
-
 def applyIDCTinAudio(cosines):
     N = len(cosines)
     signal = np.zeros(N).astype(float)
+
+    Ak = sqrt(2/N)
+    for k in range(0, N):
+        Xk = cosines[k]
+        samples = np.zeros(N).astype(float)
+        alpha = sqrt(0.5) if k == 0 else 1
+
+        for n in range(0, N):
+            samples[n] = Ak * alpha * Xk * \
+                cos(radians(((2*pi*k*n)/2*N) + ((k*pi)/2*N)))
+            signal[n] += samples[n]
+
+    # hp.coeffStrategyOne(cosines, amount)
+
+    return signal
+
+
+def applyDCTinAudio(signal):
+    N = len(signal)
+    cosines = np.zeros(N).astype(float)
+
+    Ak = sqrt(2/N)
+
     for n in range(0, N):
-        Ak = sqrt(2/N)
-        Xk = cosines[n]
+        xn = signal[n]
         samples = np.zeros(N).astype(float)
 
         for k in range(0, N):
-            alpha = sqrt(0.5) if k == 0 else 1
-            samples[k] = Ak * alpha * Xk * \
+            alpha = sqrt(0.5) if n == 0 else 1
+            samples[k] = Ak * alpha * xn * \
                 cos(radians(((2*pi*k*n)/2*N) + ((k*pi)/2*N)))
-            signal[k] = signal[k] + samples[k]
+            cosines[k] += samples[k]
 
-    return signal
+    return cosines
 
 
 if __name__ == '__main__':
     path = sys.argv[1]
     amount = sys.argv[2]
     rate, frames = hp.openAudio(path)
-    # ek = [10, 5, 8.5, 2, 1, 1.5, 0, 0.1]
+    ekCosines = [10, 5, 8.5, 2, 1, 1.5, 0, 0.1]
+    # ekSignal = [11.27835905, 5.00920938, 1.58139772, -0.10615071, -0.13384486, 2.87767395, 4.18172419, 4.06081887]
 
-    signal = applyDCTinAudio(frames, amount)
-    cosines = applyIDCTinAudio(signal)
-    # print(cosines)
-    hp.createAudio('coeff', rate, cosines)
+    signal = applyIDCTinAudio(ekCosines)
+    cosines = applyDCTinAudio(signal)
+    print(signal)
+    hp.showGraph('Cosines', signal)
+    print(cosines)
+    hp.showGraph('Signal', cosines)
+    # hp.createAudio('coeff', rate, cosines)
     # hp.showGraph('DCT', signal)
     # hp.showGraph('IDCT', cosines)
 
