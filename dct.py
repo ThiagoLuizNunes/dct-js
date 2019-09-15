4412beeb1d40722bf33cab4c701dcf181577f11f
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-09-15 12:00:59 -0300

    delete useless code

--- dct.py
+++ dct.py
@@ -7,79 +7,76 @@ def ck(k):
     return sqrt(0.5) if k == 0 else 1
 
 
-def freq(n, k):
-    return k / (2.0 * n)
+def freq(N, k):
+    return k / (2.0 * N)
 
 
-def theta(n, k):
-    return (k * pi) / (2.0 * n)
+def theta(N, k):
+    return (k * pi) / (2.0 * N)
 
 
-def applyDCT(signals):
-    n = len(signals)
-    dcLevel = np.zeros_like(signals).astype(float)
-    new_signals = np.zeros_like(signals).astype(float)
+def applyDCT(signal):
+    N = len(signal)
+    cosines = np.zeros_like(signal).astype(float)
 
     # Through cosines
-    for k in range(n):
+    for k in range(N):
         _sum = 0
-        # Through all signals
-        for i in range(n):
-            _sum += signals[i] * cos(2 * pi * freq(n, k) * i + theta(n, k))
-            if i == 0:
-              dcLevel[i] = _sum
+        # Through all signal
+        for i in range(N):
+            _sum += signal[i] * cos(2 * pi * freq(N, k) * i + theta(N, k))
 
-        new_signals[k] = sqrt(2.0 / n) * ck(k) * _sum
+        cosines[k] = sqrt(2.0 / N) * ck(k) * _sum
 
-    return new_signals
+    return cosines
 
 
-def applyIDCT(signals):
-    n = len(signals)
-    new_signals = np.zeros_like(signals).astype(float)
+def applyIDCT(cosines):
+    N = len(cosines)
+    signal = np.zeros_like(cosines).astype(float)
 
-    # Through all signals
-    for i in range(n):
+    # Through all cosines
+    for i in range(N):
         _sum = 0
         # Through cosines
-        for k in range(n):
-            _sum += ck(k) * signals[k] * cos(2 * pi * freq(n, k) * i + theta(n, k))
+        for k in range(N):
+            _sum += ck(k) * cosines[k] * cos(2 * pi * freq(N, k) * i + theta(N, k))
 
-        new_signals[i] = sqrt(2.0 / n) * _sum
+        signal[i] = sqrt(2.0 / N) * _sum
 
-    return new_signals
+    return signal
 
 
-def applyIDCT2D(signals):
-    height = signals.shape[0]
-    width = signals.shape[1]
-    new_signals_1 = np.zeros_like(signals).astype(float)
-    new_signals_2 = np.zeros_like(signals).astype(float)
+def applyIDCT2D(cosines):
+    height = cosines.shape[0]
+    width = cosines.shape[1]
+    cosines_1 = np.zeros_like(cosines).astype(float)
+    cosines_2 = np.zeros_like(cosines).astype(float)
 
     # Apply IDCT on all rows
     for h in range(height):
-        new_signals_1[h, :] = applyIDCT(signals[h, :])
+        cosines_1[h, :] = applyIDCT(cosines[h, :])
     # Apply IDCT on all columns of the previous result
     for w in range(width):
-        new_signals_2[:, w] = applyIDCT(new_signals_1[:, w])
+        cosines_2[:, w] = applyIDCT(cosines_1[:, w])
 
-    return new_signals_2
+    return cosines_2
 
 
-def applyDCT2D(signals, coef=0):
-    height = signals.shape[0]
-    width = signals.shape[1]
-    new_signals_1 = np.zeros_like(signals).astype(float)
-    new_signals_2 = np.zeros_like(signals).astype(float)
+def applyDCT2D(signal):
+    height = signal.shape[0]
+    width = signal.shape[1]
+    signal_1 = np.zeros_like(signal).astype(float)
+    signal_2 = np.zeros_like(signal).astype(float)
 
     # Apply DCT on all rows
     for h in range(height):
-        new_signals_1[h, :] = applyDCT(signals[h, :])
+        signal_1[h, :] = applyDCT(signal[h, :])
     # Apply DCT on all columns of the previous result
     for w in range(width):
-        new_signals_2[:, w] = applyDCT(new_signals_1[:, w])
+        signal_2[:, w] = applyDCT(signal_1[:, w])
 
-    return new_signals_2
+    return signal_2
 
 if __name__ == '__main__':
     path = sys.argv[1]
@@ -87,7 +84,7 @@ if __name__ == '__main__':
 
     if ('.bmp' in path) or ('.jpg' in path):
         frames = hp.openImage(path)
-        cosines = applyDCT2D(frames, amount)
+        cosines = applyDCT2D(frames)
         mostCosines2D = hp.mostImportantsCoeff2D(cosines, amount)
         hp.createImage('dct', mostCosines2D)
         signal = applyIDCT2D(mostCosines2D)
