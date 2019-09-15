ed15fd20e0be3d5703914c1b1ab755270db8dd16
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-09-15 11:35:15 -0300

    create mostImportantesCoeff to dct2D

--- dct.py
+++ dct.py
@@ -16,14 +16,8 @@ def theta(n, k):
 
 
 def applyDCT(signals):
-    """
-    One dimensional DCT
-    :param signals: 1d vector with all signals
-    :param coef: number of significant coefficients to be preserved. 0 to preserve all of them.
-    :return: 1d vector with new signals
-    """
-
     n = len(signals)
+    dcLevel = np.zeros_like(signals).astype(float)
     new_signals = np.zeros_like(signals).astype(float)
 
     # Through cosines
@@ -31,8 +25,9 @@ def applyDCT(signals):
         _sum = 0
         # Through all signals
         for i in range(n):
-            _sum += signals[i] * \
-                cos(2 * pi * freq(n, k) * i + theta(n, k))
+            _sum += signals[i] * cos(2 * pi * freq(n, k) * i + theta(n, k))
+            if i == 0:
+              dcLevel[i] = _sum
 
         new_signals[k] = sqrt(2.0 / n) * ck(k) * _sum
 
@@ -40,11 +35,6 @@ def applyDCT(signals):
 
 
 def applyIDCT(signals):
-    """
-    One dimensional IDCT
-    :param signals: 1d vector with all signals
-    :return: 1d vector with new signals
-    """
     n = len(signals)
     new_signals = np.zeros_like(signals).astype(float)
 
@@ -61,12 +51,6 @@ def applyIDCT(signals):
 
 
 def applyIDCT2D(signals):
-    """
-    Bidimensional IDCT
-    :param signals: 2d vector with all signals
-    :return: 2d vector with new signals
-    """
-
     height = signals.shape[0]
     width = signals.shape[1]
     new_signals_1 = np.zeros_like(signals).astype(float)
@@ -83,13 +67,6 @@ def applyIDCT2D(signals):
 
 
 def applyDCT2D(signals, coef=0):
-    """
-     Bidimensional DCT
-     :param signals: 2d vector with all signals
-     :param coef: number of significant coefficients to be preserved. 0 to preserve all of them.
-     :return: 2d vector with new signals
-     """
-
     height = signals.shape[0]
     width = signals.shape[1]
     new_signals_1 = np.zeros_like(signals).astype(float)
@@ -102,26 +79,6 @@ def applyDCT2D(signals, coef=0):
     for w in range(width):
         new_signals_2[:, w] = applyDCT(new_signals_1[:, w])
 
-    if coef > 0:
-        # Flat the 2D array in 1D
-        new_signals_3 = np.sort(np.array(new_signals_2).ravel())
-        # Sort the 1D array in decreasing order
-        new_signals_3 = new_signals_3[::-1]
-
-        for i in range(coef - 1, width * height):
-            new_signals_3[i] = 0
-
-        # Go back from 1D to 2D
-        new_signals_3 = np.reshape(new_signals_3, (-1, height))
-
-        for w in range(width):
-            for h in range(height):
-                for c in range(coef):
-                    if new_signals_2[h, w] == new_signals_3[c]:
-                        continue
-                    else:
-                        new_signals_2[h, w] = 0
-
     return new_signals_2
 
 if __name__ == '__main__':
@@ -130,8 +87,10 @@ if __name__ == '__main__':
 
     if ('.bmp' in path) or ('.jpg' in path):
         frames = hp.openImage(path)
-        cosines = applyDCT2D(frames)
-        signal = applyIDCT2D(cosines)
+        cosines = applyDCT2D(frames, amount)
+        mostCosines2D = hp.mostImportantsCoeff2D(cosines, amount)
+        hp.createImage('dct', mostCosines2D)
+        signal = applyIDCT2D(mostCosines2D)
         hp.createImage('coeff', signal)
     if '.wav' in path:
         rate, frames = hp.openAudio(path)
--- modules/helper.py
+++ modules/helper.py
@@ -50,6 +50,15 @@ class Cosine:
         self.value = value
         self.index = index
 
+class Cosine2D:
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+
+
+def getValue2D(elem):
+    return abs(elem.value[0]) + abs(elem.value[1]) + abs(elem.value[2]) / len(elem.value)
 
 def getValue(elem):
     return abs(elem.value)
@@ -57,6 +66,35 @@ def getValue(elem):
 
 def getIndex(elem):
     return abs(elem.index)
+
+
+def mostImportantsCoeff2D(cosines, amount):
+  row = cosines.shape[0]
+  col = cosines.shape[1]
+
+  cosinesList = []
+  mostImportants = np.zeros_like(cosines).astype(float)
+
+  if int(amount) > 0:
+      for i in range(0, row):
+          for j in range(0, col):
+            cosine = Cosine2D(cosines[i][j], i, j)
+            cosinesList.append(cosine)
+
+      cosinesList.sort(key=getValue2D)
+      cosinesList.reverse()
+
+      for x in range(0, len(cosinesList)):
+          if x > int(amount):
+              cosinesList[x].value[0] = 0
+              cosinesList[x].value[1] = 0
+              cosinesList[x].value[2] = 0
+
+      for cosine in cosinesList:
+          mostImportants[cosine.row, cosine.col] = cosine.value
+
+  return mostImportants
+
 
 
 def mostImportantsCoeff(cosines, amount):
@@ -86,4 +124,4 @@ def createAudio(name, rate, frames):
     wavfile.write('build/' + name + '.waw', rate, frames)
 
 def createImage(name, frames):
-    cv.imwrite('build/' + name + '.bmp', frames)
+    cv.imwrite('build/' + name + '.jpg', frames)
